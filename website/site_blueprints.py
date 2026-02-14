ea032b9077b199eeb2d0f1953f42c5d8c32d09c1
Steve Steyaert <sstey001@odu.edu>
2026-02-13 19:27:36 -0500

    Push

--- website/site_blueprints.py
+++ website/site_blueprints.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 blueprints = Blueprint('blueprint', __name__, static_folder='static', template_folder='templates')
 
+@blueprints.route('/')
+def home():
+    return render_template("home.html")
+
 @blueprints.route('/example')
 def example():
     return render_template("example.html")
