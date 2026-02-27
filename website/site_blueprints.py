84804751637668fdab96b90b1710a7172e53dee6
Steve Steyaert <sstey001@odu.edu>
2026-02-27 13:42:52 -0500

    Add feas 2 to navbar

--- website/site_blueprints.py
+++ website/site_blueprints.py
@@ -9,10 +9,14 @@ def home():
 @blueprint.route('/bios')
 def bios():
     return render_template('bios.html')
-@blueprint.route('/feasibility-v1')
 
+@blueprint.route('/feasibility-v1')
 def feasibility_version_one():
     return render_template('feasibility-slides-version-1.html')
+
+@blueprint.route('/feasibility-v2')
+def feasibility_version_two():
+    return render_template('feasibility-slides-version-2.html')
 
 @blueprint.route('/wip-feasibility')
 def wip_feasibility():
